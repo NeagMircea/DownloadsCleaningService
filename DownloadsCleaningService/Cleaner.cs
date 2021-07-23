fe54ffd714476c9479de25b1903c7baa92c715ae
NeagMircea <aluructosh@gmail.com>
2021-07-23 17:44:07 +0300

    uploaded project

---
+++ DownloadsCleaningService/Cleaner.cs
@@ -0,0 +1,44 @@
+﻿using Syroot.Windows.IO;
+using System;
+using System.IO;
+using System.Timers;
+
+namespace DownloadsCleaningService
+{
+    public class Cleaner
+    {
+        private readonly Timer timer;
+
+        public Cleaner()
+        {
+            timer = new Timer(1000 * 60 * 60 * 24) { AutoReset = true };
+            timer.Elapsed += OnTimeElapsed;
+        }
+
+        private void OnTimeElapsed(object sender, ElapsedEventArgs e)
+        {
+            KnownFolder downloads = new KnownFolder(KnownFolderType.Downloads);
+            string[] files = Directory.GetFiles(downloads.Path);
+
+            foreach (string file in files)
+            {
+                TimeSpan timeSpan = DateTime.Now - File.GetCreationTime(file);
+
+                if (timeSpan.Days >= 7)
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    }
+}
---
+++ DownloadsCleaningService/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Topshelf;
+
+namespace DownloadsCleaningService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<Cleaner>(s =>
+                {
+                    s.ConstructUsing(cleaner => new Cleaner());
+                    s.WhenStarted(cleaner => cleaner.Start());
+                    s.WhenStopped(cleaner => cleaner.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("CleanerService");
+                x.SetDisplayName("Cleaner Service");
+                x.SetDescription("Checks and cleans Downloads folder of files older than a week once a day.");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
